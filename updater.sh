84277b29c25eb2d83a48eb8623716374e3fa05ae
Adam Blanchet <adamblanchet@free.fr>
2019-01-24 21:31:48 +0100

    added OSX and bash updaters

---
+++ updater.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# Move all files to folder to be deleted
+mkdir old && mv * old/
+rm -rf old
+
+# Download, extract and then delete the archive
+curl -O https://codeload.github.com/Frinksy/pong-pygame/zip/master
+unzip master
+rm -rf master
+
+
+mv pong-pygame-master/* .
+rmdir pong-pygame-master
