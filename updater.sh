cb5d6c70f5e4712bfadb69fa9d190e38b4f73075
Adam Blanchet <adamblanchet@free.fr>
2019-01-24 22:09:46 +0100

    added messages

--- updater.sh
+++ updater.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
 
 # Move all files to folder to be deleted
-mkdir old && mv * old/
-rm -rf old
+echo -ne "Removing older version... \r"
+mkdir old && mv * old/ > /dev/null
+rm -rf old > /dev/null
 
 # Download, extract and then delete the archive
-curl -O https://codeload.github.com/Frinksy/pong-pygame/zip/master
-unzip master
-rm -rf master
-
-
-mv pong-pygame-master/* .
-rmdir pong-pygame-master
+echo -ne "Downloading new version.. \r"
+curl -O https://codeload.github.com/Frinksy/pong-pygame/zip/master > /dev/null
+echo -ne "Extracting... \r"
+unzip master > /dev/null
+mv pong-pygame-master/* . > /dev/null
+echo -ne "Cleaning up... \r"
+rmdir pong-pygame-master > /dev/null
+rm -rf master /dev/master > /dev/null
+echo -ne "Done!"
